e440c3a972ef8dbe07a40bb921df1baa07a7b8f6
Robert <robert.enescu@mindware-solutions.com>
2026-02-24 14:59:50 +0200

    Revert xcode.nix change

--- nix/overlays/xcode.nix
+++ nix/overlays/xcode.nix
@@ -2,9 +2,9 @@ final: prev: {
   darwin = prev.darwin.overrideScope (
     final: prev: {
       xcode_16_1 = prev.xcode.overrideAttrs (prev: {
-        outputHash = "sha256-yYg6NRRnYM/5X3hhVMfcXcdoiOV36fIongJNQ5nviD8=";
+        outputHash = "sha256-1jyRJVyOmGA7fxRwBnxSJatnOFDu01RJ9aAQXJNuWBw=";
       });
       xcode = final.xcode_16_1;
     }
-  );  
+  );
 }
